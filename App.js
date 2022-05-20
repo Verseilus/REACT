adcd76267c9190b2d2a5536c68743738cf9c0ce2
Dominik Buczny <45229269+Verseilus@users.noreply.github.com>
2022-05-20 13:32:22 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React/*, {useState}*/ from 'react'
+import WeatherWidget from './WeatherWidget';
+
+function App() {
+  return (
+    <div>
+      <WeatherWidget></WeatherWidget>
+    </div>
+  );
+}
+
+export default App;
